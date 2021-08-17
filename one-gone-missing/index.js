964a607a5594c44b18fe7b74c318e6185b43c5e1
Chris Freeman <cc.freem88@gmail.com>
2021-08-17 10:12:43 -0700

    add some comments

--- one-gone-missing/index.js
+++ one-gone-missing/index.js
@@ -1,10 +1,12 @@
 const missingNumber = (arr) => {
-  let sum = arr.reduce((a, b) => a + b);
+  // Add up all the numbers in the array
+  const sum = arr.reduce((a, b) => a + b);
+
   // Gauss's formula
-  // https://en.wikipedia.org/wiki/Gauss%27s_formula
   // computes the sum of the first `N` integers
   //  N * (N + 1) / 2
   const n = arr.length;
+  // Gauss lets us know the ideal total, we know the real total, lets do the math
   const missing = (n * (n + 1)) / 2 - sum;
   return missing;
 };
