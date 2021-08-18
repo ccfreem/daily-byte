d750f0f9393b378c34cd0ca6e660523335116f95
Chris Freeman <cc.freem88@gmail.com>
2021-08-18 14:14:06 -0700

    first pass at trie

---
+++ trie-class/index.js
@@ -0,0 +1,15 @@
+class Trie {
+  constructor() {
+    this.root = [];
+  }
+
+  insert(word) {
+    if (!this.root.includes(word)) {
+      this.root.push(word);
+    }
+  }
+  find(word) {
+    return this.root.includes(word);
+  }
+}
+module.exports = Trie;
---
+++ trie-class/index.spec.js
@@ -0,0 +1,22 @@
+const Trie = require("./index");
+
+describe("Trie", () => {
+  it("should insert", () => {
+    const trie = new Trie();
+    // Add a word
+    trie.insert("word");
+    expect(trie.root).toContain("word");
+  });
+  it("should find", () => {
+    const trie = new Trie();
+    // Add a word
+    trie.insert("word");
+    expect(trie.find("word")).toBeTruthy();
+  });
+  it("should not find if not there", () => {
+    const trie = new Trie();
+    // Add a word
+    trie.insert("word");
+    expect(trie.find("not")).toBeFalsy();
+  });
+});
